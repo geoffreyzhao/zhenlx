673f15d1cb9a9d7b21e7677aa11bf1345ef4132f
jenny.wang <jenny.wang@travelzen.com>
2015-04-03 18:10:01 +0800

    添加B2C 会员详情页
    Change-Id: I09381451eaeb3c829ffa699a0d5f0f226e336f53

--- js/maintain/common.js
+++ js/maintain/common.js
@@ -1,1 +1,30 @@
 /*  真旅行后台维护  公共功能  */
+
+$(function() {
+
+    /*  头部导航交互  开始  */
+    recoverCurrentNav();
+
+    $(".first-level-nav").mouseenter(function(){
+        $(".second-level-nav").hide();
+        $(this).find(".second-level-nav").show();
+    });
+
+    $(".first-level-nav").mouseleave(function(){
+        $(this).find(".second-level-nav").hide();
+
+        recoverCurrentNav();
+    });
+    /*  头部导航交互  结束  */
+
+    function recoverCurrentNav() {
+        $(".second-level-nav").hide();
+        $(".first-level-nav").each(function(){
+            if ($(this).hasClass("currentStairMenu")) {
+                $(this).find(".second-level-nav").show();
+            }
+        });
+    }
+    
+
+});
