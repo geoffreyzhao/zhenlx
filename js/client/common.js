2b9171e5ada2a221059d0efcdbab161b22e05729
jenny.wang <jenny.wang@travelzen.com>
2015-04-10 17:23:29 +0800

    添加 真旅行前台 控件
    Change-Id: I78b230afc1f0190b5902552c948d41d1a2c2f873

--- js/client/common.js
+++ js/client/common.js
@@ -1,1 +1,23 @@
 /*   真旅行客户端   公共功能   */
+
+$(function(){
+    // 组合input的得到和失去焦点为父元素添加或删除边框    
+    $(".componentInput input").focus(function(){
+        $(this).parents(".componentInput").addClass("addBorder")
+    });
+    $(".componentInput input").blur(function(){
+        $(this).parents(".componentInput").removeClass("addBorder")
+        });
+    
+    $(".componentInput .getVerificationCode").click(function(){
+        $(this).addClass("retry");
+        $(this).html("<span>58</span>s后重新尝试")
+    });
+
+    $(".radioContent .radio").click(function(){
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+    });
+    
+
+});
