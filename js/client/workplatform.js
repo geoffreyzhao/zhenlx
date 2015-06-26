65273a9049268edb229f6d96415c29350898cd8d
ruike.kuang <ruike.kuang@travelzen.com>
2015-06-26 18:02:58 +0800

    工作台结构修改
    Change-Id: I2c346594f37d2f5139f0701005df7cc186d028c0

--- js/client/workplatform.js
+++ js/client/workplatform.js
@@ -1,4 +1,16 @@
 $(function(){
+    $('.work-platform').height($(window).height());
+    var tabcontHeight=$(window).height()-301;
+    // $(".remind-tab-cont").eq(0).show();
+    // $(".privletter-tab-cont").eq(0).show();
+
+    $('.work-platform .remind-tab-cont').height(tabcontHeight);
+    var remindulH=$('.work-platform .remind-tab-cont ul').height();
+    if(remindulH>=tabcontHeight){
+        $('.work-platform .remind-tab-cont .more').show();
+    }
+
+
     // 选项卡点击
     $(".tab-cont").eq(0).show();
     $(".message-tip-tab li").each(function(index){
@@ -9,11 +21,15 @@ $(function(){
             $(this).addClass("current");
             $(".slidline").stop().animate({'left':169*$(this).index()},200);
             secondEllipsis();
+            TabcontControl();
         });
     });
 
+    
+
+
     // 提醒底部圆点hover
-    $(".remind-tab-cont").eq(0).show();
+   /* $(".remind-tab-cont").eq(0).show();
     $(".remind-tab-dot li").each(function(index){
         $(this).hover(function(){
             $(".remind-tab-cont").hide();
@@ -22,9 +38,9 @@ $(function(){
             $(this).addClass("current");
             // $(".slidline").animate({'left':169*$(this).index()},200);
         });
-    });
+    });*/
     // 私信底部原点hover
-    $(".privletter-tab-cont").eq(0).show();
+    /*$(".privletter-tab-cont").eq(0).show();
     $(".privletter-tab-dot li").each(function(index){
         $(this).hover(function(){
             $(".privletter-tab-cont").hide();
@@ -34,7 +50,7 @@ $(function(){
             // $(".slidline").animate({'left':169*$(this).index()},200);
             secondEllipsis();
         });
-    });
+    });*/
   
     // 工作台侧边按钮点击
     $('.workplabtn').click(function(event){
@@ -52,9 +68,26 @@ $(function(){
             } 
           }
      });
-    
+
+
+    TabcontControl();
+    function TabcontControl(){
+            $('.work-platform .tab-cont-height').each(function(){
+                var ulheight=$(this).children('ul').height();
+                var liheight=$(this).children('ul').children('li').height();
+                $(this).height(tabcontHeight);
+                if(ulheight>=tabcontHeight){
+                    delta=tabcontHeight%liheight;
+                    $(this).height(tabcontHeight-delta-15);
+                    // $(this).sibling('.more').show();
+                    $(this).sibling('.more').show();
+                }
+            });
+    }
 
 });
+
+    
 
 //  判断点击范围是否超出元素
     function isOuterTrigger(event, $ele) {
