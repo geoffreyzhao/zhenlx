3f4d8ef26e7cd981709b50b95b5bb74e8fcf7d82
ruike.kuang <ruike.kuang@travelzen.com>
2015-06-29 17:33:21 +0800

    工作台逻辑修改
    Change-Id: Iac8edce5e9b0d0debe4cb426674c84c2a89be473

--- js/client/workplatform.js
+++ js/client/workplatform.js
@@ -1,12 +1,9 @@
 $(function(){
-    $('.work-platform').height($(window).height());
-    var tabcontHeight=$(window).height()-331;
-
-    $('.work-platform .remind-tab-cont').height(tabcontHeight);
-    TabcontControl();
+    $('.work-platform').height($(window).height());   
 
     // 选项卡点击
     $(".work-platform .tab-cont").eq(0).show();
+
     $(".message-tip-tab li").each(function(index){
         $(this).click(function(){
             $(".work-platform .tab-cont").hide();
@@ -38,7 +35,17 @@ $(function(){
 
 
     // 提醒及私信模块根据内容高度展示内容
+
+    window.onresize = function(){
+        $('.work-platform').height($(window).height());
+        TabcontControl();
+    }
+
+});
+
     function TabcontControl(){
+            var tabcontHeight=$(window).height()-331;
+            $('.work-platform .remind-tab-cont').height(tabcontHeight);
             $('.work-platform .tab-cont-height').each(function(){
                 var arrliHeight=[];
                 var lis=$(this).get(0).getElementsByTagName("li");
@@ -47,24 +54,25 @@ $(function(){
                     var liheight=$(lis).eq(i).outerHeight();
                     arrliHeight.push(liheight);
                 }
-                
-                for(var i=0;i<lis.length && lisheightsum<=tabcontHeight;i++){
-                    lisheightsum+=arrliHeight[i];                    
-                    for(var j=0;j<lis.length;j++){
-                        if(j<=i){
-                           $(lis[j]).show();
-                        }else{
+
+                addHeightLoop:
+                for(var i=0;i<lis.length;i++){
+                    lisheightsum+=arrliHeight[i];
+                    $(lis).show();
+                    if(lisheightsum>tabcontHeight){
+                        for(var j=i;j<lis.length;j++){
                             $(lis[j]).hide();
                         }
+                        $(this).next(".more").show();
+                        break addHeightLoop;
+                    } else {
+                        $(this).next(".more").hide();
                     }
-                   $(this).siblings('.more').show();
                     
                 }
+
             });
     }
-
-});
-
     
 
 //  判断点击范围是否超出元素
