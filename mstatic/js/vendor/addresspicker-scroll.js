ea4567494fb7bfc23aa9065da80bae20df5dedff
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2015-08-06 14:11:03 +0800

    修改联系我们样式
    Change-Id: Idf75aea0d8b9749439db17ec4be84251a913de0f

---
+++ mstatic/js/vendor/addresspicker-scroll.js
@@ -0,0 +1,19 @@
+;(function($){
+
+	$.fn.addressPicker = function(options){
+
+		var opts = $.extend({}, options, {
+			url: '',
+			data: '', 
+			currentData: {}
+		});
+
+		return this.each(function(){
+			$this = $(this);
+
+
+		});
+		
+	}
+
+})(jQuery)
